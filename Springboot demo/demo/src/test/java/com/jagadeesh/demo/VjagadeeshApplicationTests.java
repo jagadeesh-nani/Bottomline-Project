1bfeaec6d0649a36e9a545ebe2e70751667b934e
jagadeesh-nani <jagadeeshnani350@gmail.com>
2023-12-17 13:51:27 +0530

    code status

---
+++ Springboot demo/demo/src/test/java/com/jagadeesh/demo/VjagadeeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagadeesh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjagadeeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
